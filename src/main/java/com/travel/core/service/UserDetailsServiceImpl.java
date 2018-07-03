0eea2c24cfdfbabee8af2335673d39d362e7a15f
张正十 <zhangzhengshi@juus-MacBook-puro.local>
2018-07-03 15:10:57 -0400

    Fixed unit test

--- src/main/java/com/travel/core/service/UserDetailsServiceImpl.java
+++ src/main/java/com/travel/core/service/UserDetailsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.travel.core.service;
 
+import com.travel.core.domain.Authority;
 import com.travel.core.domain.User;
+import com.travel.core.repository.AuthorityRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Locale;
 
 @Service
@@ -43,5 +46,14 @@ public class UserDetailsServiceImpl implements UserDetailsService, MessageSource
     @Override
     public void setMessageSource(MessageSource messageSource) {
         this.messageSource = messageSource;
+    }
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+    @Transactional(readOnly = true)
+    public List<Authority> findAuthoritiesByUserId(User user){
+        List<Authority> authoritiesRoles = authorityRepository.findAuthoritiesByUserId(user.getId());
+
+        return authoritiesRoles;
     }
 }
