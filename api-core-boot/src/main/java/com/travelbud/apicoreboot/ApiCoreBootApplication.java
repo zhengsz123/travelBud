77c4a9463111ffb3097bbff303cee219e866e80e
morgan <zhengsz@vt.edu>
2018-09-04 13:03:44 -0400

    Modified Spring Boot Properties

--- api-core-boot/src/main/java/com/travelbud/apicoreboot/ApiCoreBootApplication.java
+++ api-core-boot/src/main/java/com/travelbud/apicoreboot/ApiCoreBootApplication.java
@@ -3,6 +3,7 @@ package com.travelbud.apicoreboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
