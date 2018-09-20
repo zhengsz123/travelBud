2d258f1de509dfb31956d7cecc30e92e26e57b95
morgan <zhengsz@vt.edu>
2018-09-20 17:09:58 -0400

    Modified .gitignire

--- travelbudcore/src/main/java/com/travel/core/service/SmsService.java
+++ travelbudcore/src/main/java/com/travel/core/service/SmsService.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Logger;
 
 @Service
 public class SmsService {
